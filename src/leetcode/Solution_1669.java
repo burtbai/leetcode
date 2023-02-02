0ef6c8ceadc8bad6324b2cc1ed4f85d3f95d4761
bote <bote@pinduoduo.com>
2023-02-02 15:05:05 +0800

    20230131

---
+++ src/leetcode/Solution_1669.java
@@ -0,0 +1,92 @@
+package leetcode;
+
+/**
+ * @see <a href="https://leetcode.cn/problems/merge-in-between-linked-lists/">LeetCode URL</a><p>
+ * 1669. 合并两个链表
+ * 给你两个链表 list1 和 list2 ，它们包含的元素分别为 n 个和 m 个。
+ * <p>
+ * 请你将 list1 中下标从 a 到 b 的全部节点都删除，并将list2 接在被删除节点的位置。
+ * <p>
+ * 下图中蓝色边和节点展示了操作后的结果：
+ * <p>
+ * <p>
+ * 请你返回结果链表的头指针。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：list1 = [0,1,2,3,4,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+ * 输出：[0,1,2,1000000,1000001,1000002,5]
+ * 解释：我们删除 list1 中下标为 3 和 4 的两个节点，并将 list2 接在该位置。上图中蓝色的边和节点为答案链表。
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
+ * 输出：[0,1,1000000,1000001,1000002,1000003,1000004,6]
+ * 解释：上图中蓝色的边和节点为答案链表。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 3 <= list1.length <= 104
+ * 1 <= a <= b < list1.length - 1
+ * 1 <= list2.length <= 104
+ */
+class Solution_1669 {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode list2Temp = list2;
+        while (list2Temp.next != null) {
+            list2Temp = list2Temp.next;
+        }
+        ListNode list1Temp1 = list1;
+        for (int i = 0; i < a - 1; i++) {
+            list1Temp1 = list1Temp1.next;
+        }
+        ListNode list1Temp2 = list1Temp1;
+        list1Temp1.next = list2;
+        for (int i = a; i <= b; i++) {
+            list1Temp2 = list1Temp2.next;
+        }
+        list2Temp.next = list1Temp2.next;
+        return list1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
