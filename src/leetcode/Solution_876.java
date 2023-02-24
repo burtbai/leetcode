410ae72c15036924cef23c7812e0b9cc739cdb38
bote <bote@pinduoduo.com>
2023-02-24 14:27:16 +0800

    20230224

---
+++ src/leetcode/Solution_876.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+/**
+ * @see <a href="https://leetcode.cn/problems/middle-of-the-linked-list/">LeetCode URL</a><p>
+ * 876. 链表的中间结点
+ * 给你单链表的头结点 head ，请你找出并返回链表的中间结点。
+ * <p>
+ * 如果有两个中间结点，则返回第二个中间结点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[3,4,5]
+ * 解释：链表只有一个中间结点，值为 3 。
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5,6]
+ * 输出：[4,5,6]
+ * 解释：该链表有两个中间结点，值分别为 3 和 4 ，返回第二个结点。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 链表的结点数范围是 [1, 100]
+ * 1 <= Node.val <= 100
+ */
+class Solution_876 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
