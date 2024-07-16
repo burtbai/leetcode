2fba34296fab50bad51eaa84e0123e30f4f04756
burt <yongtaobai@qq.com>
2024-07-16 09:36:26 +0800

    20240716

---
+++ src/leetcode/Solution_2956.java
@@ -0,0 +1,73 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @see <a href="https://leetcode.cn/problems/find-common-elements-between-two-arrays/">LeetCode URL</a><p>
+ * 2956. 找到两个数组中的公共元素
+ * 给你两个下标从 0 开始的整数数组 nums1 和 nums2 ，它们分别含有 n 和 m 个元素。
+ * <p>
+ * 请你计算以下两个数值：
+ * <p>
+ * 统计 0 <= i < n 中的下标 i ，满足 nums1[i] 在 nums2 中 至少 出现了一次。
+ * 统计 0 <= i < m 中的下标 i ，满足 nums2[i] 在 nums1 中 至少 出现了一次。
+ * 请你返回一个长度为 2 的整数数组 answer ，按顺序 分别为以上两个数值。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [4,3,2,3,1], nums2 = [2,2,5,2,3,6]
+ * 输出：[3,4]
+ * 解释：分别计算两个数值：
+ * - nums1 中下标为 1 ，2 和 3 的元素在 nums2 中至少出现了一次，所以第一个值为 3 。
+ * - nums2 中下标为 0 ，1 ，3 和 4 的元素在 nums1 中至少出现了一次，所以第二个值为 4 。
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [3,4,2,3], nums2 = [1,5]
+ * 输出：[0,0]
+ * 解释：两个数组中没有公共元素，所以两个值都为 0 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * n == nums1.length
+ * m == nums2.length
+ * 1 <= n, m <= 100
+ * 1 <= nums1[i], nums2[i] <= 100
+ */
+class Solution_2956 {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums2) {
+            set2.add(num);
+        }
+
+        int[] ans = new int[2];
+        for (int num : nums1) {
+            if (set2.contains(num)) {
+                ans[0]++;
+            }
+        }
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                ans[1]++;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
